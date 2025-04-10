5ee7f4da770ede52afbece2d80595ca1035efc7e
Osama-Null <osamaashkanani@gmail.com>
2025-04-10 09:00:48 +0300

    Enhance models with business

--- Models/BankAccount.cs
+++ Models/BankAccount.cs
@@ -5,8 +5,9 @@ namespace growmesh_API.Models
 {
     public class BankAccount
     {
-        public int AccountId { get; set; }
+        public int BankAccountId { get; set; }
         [Range(0, double.MaxValue, ErrorMessage = "Balance must be a positive value.")]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Balance { get; set; }
 
         public List<SavingsGoal> SavingsGoals { get; set; } = new List<SavingsGoal>();
