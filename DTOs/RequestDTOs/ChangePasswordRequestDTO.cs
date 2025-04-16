613ea8c1b9e88f37f5645c6cf986b3c1a844c31a
Osama-Null <osamaashkanani@gmail.com>
2025-04-16 22:21:27 +0300

    User Controller to handle profile and password

---
+++ DTOs/RequestDTOs/ChangePasswordRequestDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace growmesh_API.DTOs.RequestDTOs
+{
+    public class ChangePasswordRequestDTO
+    {
+        [Required(ErrorMessage = "*Enter current password")]
+        [DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
+
+        [Required(ErrorMessage = "*Enter new password")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters long")]
+        public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "*Re-enter new password")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters long")]
+        [Compare("NewPassword", ErrorMessage = "*Passwords do not match")]
+        public string ConfirmNewPassword { get; set; }
+    }
+}
---
+++ DTOs/RequestDTOs/UserRequestDTO.cs
@@ -0,0 +1,24 @@
+﻿using growmesh_API.Models.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace growmesh_API.DTOs.RequestDTOs
+{
+    public class UserRequestDTO
+    {
+        [Required(ErrorMessage = "*Enter email")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        [StringLength(100, ErrorMessage = "Email cannot exceed 100 characters")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "*Enter phone number")]
+        [Phone(ErrorMessage = "Invalid phone number format")]
+        [StringLength(8, MinimumLength = 8, ErrorMessage = "Phone number must be 8 characters")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "*Enter password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public IFormFile? ProfilePicture { get; set; }
+    }
+}
